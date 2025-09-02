5b61d4dce4fc69f5b5971729be1a59e8b051ab9f
alsessions <fultonchain@gmail.com>
2025-09-02 19:26:46 -0400

    First commit.

---
+++ config/routes.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Site URL Rules
+ *
+ * You can define custom site URL rules here, which Craft will check in addition
+ * to routes defined in Settings → Routes.
+ *
+ * Read about Craft's routing behavior (and this file's structure), here:
+ * @link https://craftcms.com/docs/5.x/system/routing.html
+ */
+
+return [
+    // Business listing page
+    'business' => 'business/index',
+
+    // Individual business entries are handled by the section's URI format: business/{slug}
+    // No additional route needed as it's defined in the section configuration
+
+    // User registration and authentication routes
+    'register' => ['template' => 'users/register'],
+    'login' => ['template' => 'users/login'],
+    'logout' => ['template' => 'users/logout'],
+    'account' => ['template' => 'users/account'],
+    'account/profile' => ['template' => 'users/profile'],
+    'register/success' => ['template' => 'users/register-success'],
+];
